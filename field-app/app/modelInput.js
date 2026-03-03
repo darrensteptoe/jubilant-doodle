c59f176fc882c01f67c3eb7adc1a846b5ccfac60
darrensteptoe <me@darrensteptoe.com>
2026-03-03 13:36:08 -0600

    Delete field-app/app directory

--- field-app/app/modelInput.js
+++
@@ -1,5 +0,0 @@
-import { buildModelInputFromSnapshot } from "../core/modelInput.js";
-
-export function buildModelInputFromState(state, safeNumFn){
-  return buildModelInputFromSnapshot(state, safeNumFn);
-}
