9cf5daf1188e7a9c95e60503e4b0a75b8a797638
darrensteptoe <me@darrensteptoe.com>
2026-03-07 01:16:31 -0600

    Delete field-app/js/timelineOptimizer.js

--- field-app/js/timelineOptimizer.js
+++
@@ -1,4 +0,0 @@
-// Compatibility shim: legacy root import path.
-// New internal imports should target "./core/..." directly.
-// js/timelineOptimizer.js
-export * from "./core/timelineOptimizer.js";
