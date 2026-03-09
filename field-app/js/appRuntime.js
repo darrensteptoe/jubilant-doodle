becc26c728b45373947b6fbee7c8a264a691572f
darrensteptoe <me@darrensteptoe.com>
2026-03-09 16:25:01 -0500

    Add files via upload

--- field-app/js/appRuntime.js
+++ field-app/js/appRuntime.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { engine } from "./engine.js";
+import { engine } from "./engine.js?v=20260309-census-phase1-5";
 import {
   computeCapacityContacts as coreComputeCapacityContacts,
   computeCapacityBreakdown as coreComputeCapacityBreakdown,
