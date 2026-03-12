64b8b12fde0fd0ff3008876d845ece17713d7c87
darrensteptoe <me@darrensteptoe.com>
2026-03-12 10:15:17 -0500

    Add files via upload

--- field-app/js/app/uiBindings.js
+++ field-app/js/app/uiBindings.js
@@ -6,25 +6,28 @@ export function applyStateToUiBindings({
   syncGotvModeUI,
   applyThemeFromState,
 }){
-  els.scenarioName.value = state.scenarioName || "";
-  els.raceType.value = state.raceType || "state_leg";
-  els.electionDate.value = state.electionDate || "";
-  els.weeksRemaining.value = state.weeksRemaining || "";
-  els.mode.value = state.mode || "persuasion";
+  const setValue = (el, v) => { if (el) el.value = v ?? ""; };
+  const setChecked = (el, v) => { if (el) el.checked = !!v; };
 
-  els.universeBasis.value = state.universeBasis || "registered";
-  els.universeSize.value = state.universeSize ?? "";
-  els.sourceNote.value = state.sourceNote || "";
+  setValue(els.scenarioName, state.scenarioName || "");
+  setValue(els.raceType, state.raceType || "state_leg");
+  setValue(els.electionDate, state.electionDate || "");
+  setValue(els.weeksRemaining, state.weeksRemaining || "");
+  setValue(els.mode, state.mode || "persuasion");
 
-  els.turnoutA.value = state.turnoutA ?? "";
-  els.turnoutB.value = state.turnoutB ?? "";
-  els.bandWidth.value = state.bandWidth ?? "";
+  setValue(els.universeBasis, state.universeBasis || "registered");
+  setValue(els.universeSize, state.universeSize ?? "");
+  setValue(els.sourceNote, state.sourceNote || "");
 
-  els.undecidedPct.value = state.undecidedPct ?? "";
-  els.undecidedMode.value = state.undecidedMode || "proportional";
+  setValue(els.turnoutA, state.turnoutA ?? "");
+  setValue(els.turnoutB, state.turnoutB ?? "");
+  setValue(els.bandWidth, state.bandWidth ?? "");
 
-  if (els.persuasionPct) els.persuasionPct.value = state.persuasionPct ?? "";
-  if (els.earlyVoteExp) els.earlyVoteExp.value = state.earlyVoteExp ?? "";
+  setValue(els.undecidedPct, state.undecidedPct ?? "");
+  setValue(els.undecidedMode, state.undecidedMode || "proportional");
+
+  setValue(els.persuasionPct, state.persuasionPct ?? "");
+  setValue(els.earlyVoteExp, state.earlyVoteExp ?? "");
 
   if (els.goalSupportIds) els.goalSupportIds.value = state.goalSupportIds ?? "";
   if (els.supportRatePct) els.supportRatePct.value = state.supportRatePct ?? "";
@@ -33,7 +36,7 @@ export function applyStateToUiBindings({
   if (els.hoursPerShift) els.hoursPerShift.value = state.hoursPerShift ?? "";
   if (els.shiftsPerVolunteerPerWeek) els.shiftsPerVolunteerPerWeek.value = state.shiftsPerVolunteerPerWeek ?? "";
 
-  if (els.universe16Enabled) els.universe16Enabled.checked = !!state.universeLayerEnabled;
+  setChecked(els.universe16Enabled, !!state.universeLayerEnabled);
   if (els.universe16DemPct) els.universe16DemPct.value = state.universeDemPct ?? "";
   if (els.universe16RepPct) els.universe16RepPct.value = state.universeRepPct ?? "";
   if (els.universe16NpaPct) els.universe16NpaPct.value = state.universeNpaPct ?? "";
@@ -48,13 +51,13 @@ export function applyStateToUiBindings({
   if (els.callsPerHour3) els.callsPerHour3.value = state.callsPerHour3 ?? "";
   if (els.turnoutReliabilityPct) els.turnoutReliabilityPct.value = state.turnoutReliabilityPct ?? "";
 
-  if (els.turnoutEnabled) els.turnoutEnabled.checked = !!state.turnoutEnabled;
+  setChecked(els.turnoutEnabled, !!state.turnoutEnabled);
   if (els.turnoutBaselinePct) els.turnoutBaselinePct.value = state.turnoutBaselinePct ?? "";
   if (els.turnoutTargetOverridePct) els.turnoutTargetOverridePct.value = state.turnoutTargetOverridePct ?? "";
   if (els.gotvMode) els.gotvMode.value = state.gotvMode || "basic";
   if (els.gotvLiftPP) els.gotvLiftPP.value = state.gotvLiftPP ?? "";
   if (els.gotvMaxLiftPP) els.gotvMaxLiftPP.value = state.gotvMaxLiftPP ?? "";
-  if (els.gotvDiminishing) els.gotvDiminishing.checked = !!state.gotvDiminishing;
+  setChecked(els.gotvDiminishing, !!state.gotvDiminishing);
   if (els.gotvLiftMin) els.gotvLiftMin.value = state.gotvLiftMin ?? "";
   if (els.gotvLiftMode) els.gotvLiftMode.value = state.gotvLiftMode ?? "";
   if (els.gotvLiftMax) els.gotvLiftMax.value = state.gotvLiftMax ?? "";
@@ -87,49 +90,49 @@ export function applyStateToUiBindings({
   syncMcModeUI();
   syncGotvModeUI();
 
-  if (els.roiDoorsEnabled) els.roiDoorsEnabled.checked = !!state.budget?.tactics?.doors?.enabled;
+  setChecked(els.roiDoorsEnabled, !!state.budget?.tactics?.doors?.enabled);
   if (els.roiDoorsCpa) els.roiDoorsCpa.value = state.budget?.tactics?.doors?.cpa ?? "";
   if (els.roiDoorsKind) els.roiDoorsKind.value = state.budget?.tactics?.doors?.kind || "persuasion";
   if (els.roiDoorsCr) els.roiDoorsCr.value = state.budget?.tactics?.doors?.crPct ?? "";
   if (els.roiDoorsSr) els.roiDoorsSr.value = state.budget?.tactics?.doors?.srPct ?? "";
-  if (els.roiPhonesEnabled) els.roiPhonesEnabled.checked = !!state.budget?.tactics?.phones?.enabled;
+  setChecked(els.roiPhonesEnabled, !!state.budget?.tactics?.phones?.enabled);
   if (els.roiPhonesCpa) els.roiPhonesCpa.value = state.budget?.tactics?.phones?.cpa ?? "";
   if (els.roiPhonesKind) els.roiPhonesKind.value = state.budget?.tactics?.phones?.kind || "persuasion";
   if (els.roiPhonesCr) els.roiPhonesCr.value = state.budget?.tactics?.phones?.crPct ?? "";
   if (els.roiPhonesSr) els.roiPhonesSr.value = state.budget?.tactics?.phones?.srPct ?? "";
-  if (els.roiTextsEnabled) els.roiTextsEnabled.checked = !!state.budget?.tactics?.texts?.enabled;
+  setChecked(els.roiTextsEnabled, !!state.budget?.tactics?.texts?.enabled);
   if (els.roiTextsCpa) els.roiTextsCpa.value = state.budget?.tactics?.texts?.cpa ?? "";
   if (els.roiTextsKind) els.roiTextsKind.value = state.budget?.tactics?.texts?.kind || "persuasion";
   if (els.roiTextsCr) els.roiTextsCr.value = state.budget?.tactics?.texts?.crPct ?? "";
   if (els.roiTextsSr) els.roiTextsSr.value = state.budget?.tactics?.texts?.srPct ?? "";
   if (els.roiOverheadAmount) els.roiOverheadAmount.value = state.budget?.overheadAmount ?? "";
-  if (els.roiIncludeOverhead) els.roiIncludeOverhead.checked = !!state.budget?.includeOverhead;
+  setChecked(els.roiIncludeOverhead, !!state.budget?.includeOverhead);
 
   if (els.optMode) els.optMode.value = state.budget?.optimize?.mode || "budget";
   if (els.optObjective) els.optObjective.value = state.budget?.optimize?.objective || "net";
-  if (els.tlOptEnabled) els.tlOptEnabled.checked = !!state.budget?.optimize?.tlConstrainedEnabled;
+  setChecked(els.tlOptEnabled, !!state.budget?.optimize?.tlConstrainedEnabled);
   if (els.tlOptObjective) els.tlOptObjective.value = state.budget?.optimize?.tlConstrainedObjective || "max_net";
   if (els.optBudget) els.optBudget.value = state.budget?.optimize?.budgetAmount ?? "";
   if (els.optCapacity) els.optCapacity.value = state.budget?.optimize?.capacityAttempts ?? "";
   if (els.optStep) els.optStep.value = state.budget?.optimize?.step ?? 25;
-  if (els.optUseDecay) els.optUseDecay.checked = !!state.budget?.optimize?.useDecay;
+  setChecked(els.optUseDecay, !!state.budget?.optimize?.useDecay);
 
-  if (els.timelineEnabled) els.timelineEnabled.checked = !!state.timelineEnabled;
+  setChecked(els.timelineEnabled, !!state.timelineEnabled);
   if (els.timelineActiveWeeks) els.timelineActiveWeeks.value = state.timelineActiveWeeks ?? "";
   if (els.timelineGotvWeeks) els.timelineGotvWeeks.value = state.timelineGotvWeeks ?? "";
   if (els.timelineStaffCount) els.timelineStaffCount.value = state.timelineStaffCount ?? "";
   if (els.timelineStaffHours) els.timelineStaffHours.value = state.timelineStaffHours ?? "";
   if (els.timelineVolCount) els.timelineVolCount.value = state.timelineVolCount ?? "";
   if (els.timelineVolHours) els.timelineVolHours.value = state.timelineVolHours ?? "";
-  if (els.timelineRampEnabled) els.timelineRampEnabled.checked = !!state.timelineRampEnabled;
+  setChecked(els.timelineRampEnabled, !!state.timelineRampEnabled);
   if (els.timelineRampMode) els.timelineRampMode.value = state.timelineRampMode || "linear";
   if (els.timelineDoorsPerHour) els.timelineDoorsPerHour.value = state.timelineDoorsPerHour ?? "";
   if (els.timelineCallsPerHour) els.timelineCallsPerHour.value = state.timelineCallsPerHour ?? "";
   if (els.timelineTextsPerHour) els.timelineTextsPerHour.value = state.timelineTextsPerHour ?? "";
 
-  if (els.toggleAdvDiag) els.toggleAdvDiag.checked = !!state.ui?.advDiag;
+  setChecked(els.toggleAdvDiag, !!state.ui?.advDiag);
   if (els.advDiagBox) els.advDiagBox.hidden = !state.ui?.advDiag;
-  if (els.toggleTraining) els.toggleTraining.checked = !!state.ui?.training;
+  setChecked(els.toggleTraining, !!state.ui?.training);
 
   document.body.classList.toggle("training", !!state.ui?.training);
   applyThemeFromState();
